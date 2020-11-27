7386500461afbc72a1cd4d6af15e104916834206
Paul FERA <paulfera17@gmail.com>
2020-11-27 18:17:53 +0100

    All files

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,35 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+    Fingerprint.isAvailable(isAvailableSuccess, isAvailableError);
+}
+
+function isAvailableSuccess(result) {
+    /*
+    result depends on device and os.
+    iPhone X will return 'face' other Android or iOS devices will return 'finger' Android P+ will return 'biometric'
+    */
+    console.log(result)
+    alert("Fingerprint available");
+    Fingerprint.show({
+        description: "Some biometric description"
+    }, successAuth, errorAuth);
+}
+
+function isAvailableError(error) {
+    // 'error' will be an object with an error code and message
+    console.log(error.message);
+    alert(error.message);
+}
+
+function successAuth(){
+    alert("Authentification par empreinte digitale validée");
+}
+
+function errorAuth(error){
+    alert("Authentication invalid " + error.message);
+}
